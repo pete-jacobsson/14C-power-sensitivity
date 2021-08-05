b19de9850151f446d8032cf07352150deba6fcf6
Pete Jacobsson <pt.jacobsson@gmail.com>
2021-08-05 11:38:05 +0100

    Add commenting and finish axes selection

--- sensitivity_shiny/server.R
+++ sensitivity_shiny/server.R
@@ -1,7 +1,11 @@
 shinyServer(function(input, output) {
+
+#Singles011--------------------------------------------------------------------  
   output$x_axis_text_011 <- renderText({
     paste0("X axis is:", input$x_axis_text_011)
   })
-  
+  output$y_axis_text_011 <- renderText({
+    paste0("y axis is:", input$y_axis_text_011)
+  })
   
 })
--- sensitivity_shiny/ui.R
+++ sensitivity_shiny/ui.R
@@ -5,6 +5,7 @@ shinyUI(
     title = "14C Sensitivity",
     tabPanel(title = "Home"),
     navbarMenu(title = "EDA",
+#Singles011 EDA----------------------------------------------------------------
                tabPanel(title = "Single Calibrations 011",
                         sidebarLayout(
                           sidebarPanel(
@@ -13,29 +14,60 @@ shinyUI(
                               inputId = "x_axis_text_011",
                               label = "Choose a column from the data",
                               data = singles_011_results
-                            )),
+                            ),
+                            varSelectInput(
+                              inputId = "y_axis_text_011",
+                              label = "Choose a column from the data",
+                              data = singles_011_results
+                            )
+                            
+                            ),
                           mainPanel(
-                            textOutput("x_axis_text_011"))
+                            textOutput("x_axis_text_011"),
+                            textOutput("y_axis_text_011"))
                         )
                         ),
+#WMDs021 EDA-------------------------------------------------------------------
                tabPanel(title = "Wiggle-match Dates 021"),
+#WMDs023 EDA-------------------------------------------------------------------
                tabPanel(title = "Wiggle-match Dates 023"),
+#WMDs024 EDA-------------------------------------------------------------------
                tabPanel(title = "Wiggle-match Dates 024"),
+#WMDs025 EDA-------------------------------------------------------------------
                tabPanel(title = "Wiggle-match Dates 025"),
+#Seqs031 EDA-------------------------------------------------------------------
                tabPanel(title = "Uniform Bound Sequences 031"),
+#Seqs033 EDA-------------------------------------------------------------------
                tabPanel(title = "Uniform Bound Sequences 033")
                ),
+
+
+
     navbarMenu(title = "Maximum Permissible Error",
+#Singles011 EDA----------------------------------------------------------------               
                tabPanel(title = "Single Calibrations 011"),
+#WMDs021 EDA-------------------------------------------------------------------              
                tabPanel(title = "Wiggle match Dates 021"),
+#WMDs022 EDA-------------------------------------------------------------------              
                tabPanel(title = "Wiggle match Dates 022"),
+#WMDs023 EDA-------------------------------------------------------------------               
                tabPanel(title = "Wiggle match Dates 023"),
+#WMDs024 EDA-------------------------------------------------------------------              
                tabPanel(title = "Wiggle match Dates 024"),
+#WMDs025 EDA-------------------------------------------------------------------               
                tabPanel(title = "Wiggle match Dates 025"),
+#Seqs031 EDA-------------------------------------------------------------------               
                tabPanel(title = "Uniform Bound Sequences 031"),
+#Seqs032 EDA-------------------------------------------------------------------                 
                tabPanel(title = "Uniform Bound Sequences 032"),
+#Seqs033 EDA-------------------------------------------------------------------                 
                tabPanel(title = "Uniform Bound Sequences 033")
                ),
+
+
+
+
+
     navbarMenu(title = "Modelling")
     
   )
