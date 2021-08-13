94071d95b77bfb40ee725ebd85bb727e0630ae83
Pete Jacobsson <pt.jacobsson@gmail.com>
2021-08-13 14:45:47 +0100

    Set up singles 011 mpe ui

--- sensitivity_shiny/server.R
+++ sensitivity_shiny/server.R
@@ -1,40 +1,40 @@
 shinyServer(function(input, output) {
   
   #Singles011--------------------------------------------------------------------  
-  output$plot_011 <- renderPlot({
+  output$plot_011_eda <- renderPlot({
     
     #Prep sims: filter fit slider choice and round where applicable------------
     sim_results_filtered <- singles_011_results %>%
       filter( 
-        target_year >= input$target_year_011[1] & 
-          target_year <= input$target_year_011[2],
-        offset_magnitude >= input$offset_magnitude_011[1] & 
-          offset_magnitude <= input$offset_magnitude_011[2],
-        measurement_error >= input$measurement_error_011[1] & 
-          measurement_error <= input$measurement_error_011[2]
+        target_year >= input$target_year_011_eda[1] & 
+          target_year <= input$target_year_011_eda[2],
+        offset_magnitude >= input$offset_magnitude_011_eda[1] & 
+          offset_magnitude <= input$offset_magnitude_011_eda[2],
+        measurement_error >= input$measurement_error_011_eda[1] & 
+          measurement_error <= input$measurement_error_011_eda[2]
       )
     
     #Get the x_axis_label
     x_axis_label <- case_when(
-      input$x_axis_011 == "target_year" ~ "Target year (cal BP)",
-      input$x_axis_011 == "measurement_error" ~ "Measurement error (14C yrs)",
-      input$x_axis_011 == "offset_magnitude" ~ "Offset magnitude (14C yrs)"
+      input$x_axis_011_eda == "target_year" ~ "Target year (cal BP)",
+      input$x_axis_011_eda == "measurement_error" ~ "Measurement error (14C yrs)",
+      input$x_axis_011_eda == "offset_magnitude" ~ "Offset magnitude (14C yrs)"
     )
     
     #Fork 1: plot ratio accurate-----------------------------------------------
     #Group on the desired parameter
-    if (input$predicted_variable_011 == "accuracy") {
+    if (input$predicted_variable_011_eda == "accuracy") {
       
       #Step 1: group the data
       sim_results_grouped <- sim_results_filtered %>%
         #1.1 generate the bins to group on
         mutate(
-          target_year  = plyr::round_any(target_year, input$rounding_slider*100),
-          measurement_error = plyr::round_any(measurement_error, input$rounding_slider),
-          offset_magnitude  = plyr::round_any(offset_magnitude, input$rounding_slider)
+          target_year  = plyr::round_any(target_year, input$rounding_slider_011_eda*100),
+          measurement_error = plyr::round_any(measurement_error, input$rounding_slider_011_eda),
+          offset_magnitude  = plyr::round_any(offset_magnitude, input$rounding_slider_011_eda)
         ) %>%
         #1.2 group and summarise (extract average accuracy over a given bin)
-        group_by(!!sym(input$x_axis_011)) %>%
+        group_by(!!sym(input$x_axis_011_eda)) %>%
         summarise(
           ratio_68 = mean(accuracy_68),
           ratio_95 = mean(accuracy_95)
@@ -48,7 +48,7 @@ shinyServer(function(input, output) {
       colnames(sim_results_grouped)[1] <- "x_axis"
     
       #Step 3: filter away the hpd area not chosen on the radio button
-      if (input$hpd_area_011 == "hpd_68") {
+      if (input$hpd_area_011_eda == "hpd_68") {
         sim_results_grouped <- sim_results_grouped %>%
           filter(hpd_area == "ratio_68") %>%
           select(-hpd_area)
@@ -58,14 +58,14 @@ shinyServer(function(input, output) {
           select(-hpd_area)
       }
       
-      #Step 4: set up the width of bin and the position of the geom_vline
-      if (input$x_axis_011 ==  "target_year") {
-        width_of_bin <- input$rounding_slider*100
+      #Step 4: set up the width of bin and the position of the geom_hline
+      if (input$x_axis_011_eda ==  "target_year") {
+        width_of_bin <- input$rounding_slider_011_eda*100
       } else {
-        width_of_bin = input$rounding_slider
+        width_of_bin = input$rounding_slider_011_eda
       }
       
-      if (input$hpd_area_011 == "hpd_68") {
+      if (input$hpd_area_011_eda == "hpd_68") {
         hline <- 0.68
       } else {
         hline <- 0.95
@@ -91,7 +91,7 @@ shinyServer(function(input, output) {
                      values_to = "off_target")
       
       #Step 2: filter away the hpd area not chosen on the radio button
-      if (input$hpd_area_011 == "hpd_68") {
+      if (input$hpd_area_011_eda == "hpd_68") {
         sim_results_filtered <- sim_results_filtered %>%
           filter(hpd_area == "off_target_68") %>%
           select(-hpd_area) 
@@ -102,7 +102,7 @@ shinyServer(function(input, output) {
       }
       
       #Step 3: shift the off-targetvalues to neg for neg offsets for target_yr observations
-      if (input$x_axis_011 == "target_year") {
+      if (input$x_axis_011_eda == "target_year") {
         sim_results_filtered <- sim_results_filtered %>%
           mutate(off_target = if_else(offset_magnitude < 0,
                                       off_target * -1,
@@ -111,23 +111,23 @@ shinyServer(function(input, output) {
       
       #Step 4: Do the basic plotting
       # off_target_plot <- sim_results_filtered %>%
-      #   ggplot(aes(x = !!sym(input$x_axis_011), y = off_target)) +
+      #   ggplot(aes(x = !!sym(input$x_axis_011_eda), y = off_target)) +
       #   xlab(x_axis_label) +
       #   ylab("Off-target magnitude") +
       #   theme_bw()
       
       #Step 5: Color on demand
-      if (input$off_target_color == "nope") {
+      if (input$off_target_color_011_eda == "nope") {
         off_target_plot <- sim_results_filtered %>%
-          ggplot(aes(x = !!sym(input$x_axis_011), y = off_target)) +
+          ggplot(aes(x = !!sym(input$x_axis_011_eda), y = off_target)) +
           geom_point() +
           xlab(x_axis_label) +
           ylab("Off-target magnitude") +
           theme_bw()
       } else {
         off_target_plot <- sim_results_filtered %>%
-          ggplot(aes(x = !!sym(input$x_axis_011), y = off_target)) +
-          geom_point(aes(col = abs(!!sym(input$off_target_color)))) +
+          ggplot(aes(x = !!sym(input$x_axis_011_eda), y = off_target)) +
+          geom_point(aes(col = abs(!!sym(input$off_target_color_011_eda)))) +
           xlab(x_axis_label) +
           ylab("Off-target magnitude") +
           theme_bw()
@@ -136,7 +136,7 @@ shinyServer(function(input, output) {
       
       #Step 6: Remove negatives from the y axis for target-yea x-axis
       
-      if (input$x_axis_011 == "target_year") {
+      if (input$x_axis_011_eda == "target_year") {
         off_target_plot +
           scale_y_continuous(labels = c(300, 200, 100,  0, 100, 200, 300),
                              breaks = c(-300, -200, -100,  0, 100, 200, 300),
--- sensitivity_shiny/ui.R
+++ sensitivity_shiny/ui.R
@@ -12,25 +12,25 @@ shinyUI(
                             "",
                             #Select axes---------------------------------------
                             selectInput(
-                              inputId = "x_axis_011",
+                              inputId = "x_axis_011_eda",
                               label = "Choose variable:",
                               choices = names(singles_011_results[,c(1, 3, 4)])
                             ),
                             radioButtons(
-                              inputId = "predicted_variable_011",
+                              inputId = "predicted_variable_011_eda",
                               label = "Choose variable:",
                               choices = list("Accuracy" = "accuracy",
                                              "Off-target" = "off_target")
                             ),
                             radioButtons(
-                              inputId = "hpd_area_011",
+                              inputId = "hpd_area_011_eda",
                               label = "Select HPD Area:",
                               choices = list("68% probability" = "hpd_68",
                                              "95% probability" = "hpd_95")
                             ),
                             #Set up color control for off-target graphs--------
                             radioButtons(
-                              inputId = "off_target_color",
+                              inputId = "off_target_color_011_eda",
                               label = "Colour code off-target graph?",
                               choices = list("No" = "nope",
                                              "Measurement error" = "measurement_error",
@@ -38,7 +38,7 @@ shinyUI(
                             ),
                             #Set up the rounding slider------------------------
                             sliderInput(
-                              inputId = "rounding_slider",
+                              inputId = "rounding_slider_011_eda",
                               label = "Select bin width",
                               min = 1,
                               max = 10,
@@ -46,20 +46,20 @@ shinyUI(
                             ),
                             #Set up sliders------------------------------------
                             sliderInput(
-                              inputId = "offset_magnitude_011",
+                              inputId = "offset_magnitude_011_eda",
                               label = "Offset magnitude (14C yrs)",
                               min = -50,
                               max = 50,
                               value = c(-50, 50)),
                             sliderInput(
-                              inputId = "measurement_error_011",
+                              inputId = "measurement_error_011_eda",
                               label = "Measurement error (14C yrs 1-s)",
                               min = 8,
                               max = 32,
                               value = c(8, 32)
                             ),
                             sliderInput(
-                              inputId = "target_year_011",
+                              inputId = "target_year_011_eda",
                               label = "Target year (cal BP)",
                               min = 0, 
                               max = 12310,
@@ -68,12 +68,7 @@ shinyUI(
                             ),
                           #Main panel------------------------------------------
                           mainPanel(
-                            verbatimTextOutput("offset_magnitude_011"),
-                            verbatimTextOutput("measurement_error_011"),
-                            verbatimTextOutput("target_year_011"),
-                            verbatimTextOutput("button_1"),
-                            verbatimTextOutput("button_2"),
-                            plotOutput("plot_011"))
+                            plotOutput("plot_011_eda"))
                         )
                         ),
 #WMDs021 EDA-------------------------------------------------------------------
@@ -94,7 +89,70 @@ shinyUI(
 
     navbarMenu(title = "Maximum Permissible Error",
 #Singles011 MPE----------------------------------------------------------------               
-               tabPanel(title = "Single Calibrations 011"),
+               tabPanel(title = "Single Calibrations 011",
+                        sidebarLayout(
+                          sidebarPanel(
+                            "",
+                            #Select axes---------------------------------------
+                            selectInput(
+                              inputId = "x_axis_011_mpe",
+                              label = "Choose variable:",
+                              choices = names(singles_011_results[,c(1, 3, 4)])
+                            ),
+                            radioButtons(
+                              inputId = "hpd_area_011_mpe",
+                              label = "Select HPD Area:",
+                              choices = list("68% probability" = "hpd_68",
+                                             "95% probability" = "hpd_95")
+                            ),
+                            #MPE controls--------------------------------------
+                            sliderInput(
+                              inputId = "singles_011_say_mpe",
+                              label = "What is your MPE?",
+                              min = 0,
+                              max = 50,
+                              value = 5
+                            ),
+                            sliderInput(
+                              inputId = "singles_011_mpe_tolerance",
+                              label = "What is your MPE tolerance?",
+                              min = 0,
+                              max = 1,
+                              value = 0.05
+                            ),
+                            
+                            #Sliders-------------------------------------------
+                            sliderInput(
+                              inputId = "rounding_slider_011_mpe",
+                              label = "Select bin width",
+                              min = 1,
+                              max = 10,
+                              value = 1
+                            ),
+                            sliderInput(
+                              inputId = "offset_magnitude_011_mpe",
+                              label = "Offset magnitude (14C yrs)",
+                              min = -50,
+                              max = 50,
+                              value = c(-50, 50)),
+                            sliderInput(
+                              inputId = "measurement_error_011_mpe",
+                              label = "Measurement error (14C yrs 1-s)",
+                              min = 8,
+                              max = 32,
+                              value = c(8, 32)
+                            ),
+                            sliderInput(
+                              inputId = "target_year_011_mpe",
+                              label = "Target year (cal BP)",
+                              min = 0, 
+                              max = 12310,
+                              value = c(0, 12310)
+                            )
+                          ),
+                          mainPanel()
+                        )
+                        ),
 #WMDs021 EDA-------------------------------------------------------------------              
                tabPanel(title = "Wiggle match Dates 021"),
 #WMDs022 EDA-------------------------------------------------------------------              
