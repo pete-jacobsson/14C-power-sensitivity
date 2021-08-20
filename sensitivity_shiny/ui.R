e8566c5b929a46bf7ea57f79606e3cfdbe4d54d9
Pete Jacobsson <pt.jacobsson@gmail.com>
2021-08-20 12:50:25 +0100

    Restructure app ui

--- sensitivity_shiny/ui.R
+++ sensitivity_shiny/ui.R
@@ -4,9 +4,9 @@ shinyUI(
   navbarPage(
     title = "14C Sensitivity",
     tabPanel(title = "Home"),
-    navbarMenu(title = "EDA",
-#Singles011 EDA----------------------------------------------------------------
-               tabPanel(title = "Single Calibrations 011",
+    navbarMenu(title = "Single calibrations",
+#Singles011 EDA 1: histograms and off target magnitudes------------------------
+               tabPanel(title = "Accuracy histograms and off-target magnitudes",
                         sidebarLayout(
                           sidebarPanel(
                             "",
@@ -72,113 +72,112 @@ shinyUI(
                             )
                         )
                         ),
-#WMDs021 EDA-------------------------------------------------------------------
-               tabPanel(title = "Wiggle-match Dates 021"),
-#WMDs023 EDA-------------------------------------------------------------------
-               tabPanel(title = "Wiggle-match Dates 023"),
-#WMDs024 EDA-------------------------------------------------------------------
-               tabPanel(title = "Wiggle-match Dates 024"),
-#WMDs025 EDA-------------------------------------------------------------------
-               tabPanel(title = "Wiggle-match Dates 025"),
-#Seqs031 EDA-------------------------------------------------------------------
-               tabPanel(title = "Uniform Bound Sequences 031"),
-#Seqs033 EDA-------------------------------------------------------------------
-               tabPanel(title = "Uniform Bound Sequences 033")
-               ),
-
-
-
-    navbarMenu(title = "Maximum Permissible Error",
-#Singles011 MPE----------------------------------------------------------------               
-               tabPanel(title = "Single Calibrations 011",
+#Singles011 EDA 2: heatmaps----------------------------------------------------
+               tabPanel(title = "Heatmaps"),
+#Singles011 MPE----------------------------------------------------------------
+               tabPanel(title = "Maximum permissable errors",
                         sidebarLayout(
                           sidebarPanel(
                             "",
                             #Select axes---------------------------------------
-                            selectInput(
-                              inputId = "x_axis_011_mpe",
-                              label = "Choose variable:",
-                              choices = names(singles_011_results[,c(1, 3, 4)])
-                            ),
-                            radioButtons(
-                              inputId = "hpd_area_011_mpe",
-                              label = "Select HPD Area:",
-                              choices = list("68% probability" = "off_target_68",
-                                             "95% probability" = "off_target_95")
-                            ),
-                            #MPE controls--------------------------------------
-                            sliderInput(
-                              inputId = "singles_011_say_mpe",
-                              label = "What is your MPE?",
-                              min = 0,
-                              max = 50,
-                              value = 5
-                            ),
-                            sliderInput(
-                              inputId = "singles_011_tolerance_mpe",
-                              label = "What is your MPE tolerance?",
-                              min = 0,
-                              max = 1,
-                              value = 0.05
-                            ),
-                            
-                            #Sliders-------------------------------------------
-                            sliderInput(
-                              inputId = "rounding_slider_011_mpe",
-                              label = "Select bin width",
-                              min = 1,
-                              max = 10,
-                              value = 1
-                            ),
-                            sliderInput(
-                              inputId = "offset_magnitude_011_mpe",
-                              label = "Offset magnitude (14C yrs)",
-                              min = -50,
-                              max = 50,
-                              value = c(-50, 50)),
-                            sliderInput(
-                              inputId = "measurement_error_011_mpe",
-                              label = "Measurement error (14C yrs 1-s)",
-                              min = 8,
-                              max = 32,
-                              value = c(8, 32)
-                            ),
-                            sliderInput(
-                              inputId = "target_year_011_mpe",
-                              label = "Target year (cal BP)",
-                              min = 0, 
-                              max = 12310,
-                              value = c(0, 12310)
-                            )
+                          selectInput(
+                            inputId = "x_axis_011_mpe",
+                            label = "Choose variable:",
+                            choices = names(singles_011_results[,c(1, 3, 4)])
+                          ),
+                          radioButtons(
+                            inputId = "hpd_area_011_mpe",
+                            label = "Select HPD Area:",
+                            choices = list("68% probability" = "off_target_68",
+                                           "95% probability" = "off_target_95")
+                          ),
+                          #MPE controls--------------------------------------
+                          sliderInput(
+                            inputId = "singles_011_say_mpe",
+                            label = "What is your MPE?",
+                            min = 0,
+                            max = 50,
+                            value = 5
+                          ),
+                          sliderInput(
+                            inputId = "singles_011_tolerance_mpe",
+                            label = "What is your MPE tolerance?",
+                            min = 0,
+                            max = 1,
+                            value = 0.05
+                          ),
+                          
+                          #Sliders-------------------------------------------
+                          sliderInput(
+                            inputId = "rounding_slider_011_mpe",
+                            label = "Select bin width",
+                            min = 1,
+                            max = 10,
+                            value = 1
+                          ),
+                          sliderInput(
+                            inputId = "offset_magnitude_011_mpe",
+                            label = "Offset magnitude (14C yrs)",
+                            min = -50,
+                            max = 50,
+                            value = c(-50, 50)),
+                          sliderInput(
+                            inputId = "measurement_error_011_mpe",
+                            label = "Measurement error (14C yrs 1-s)",
+                            min = 8,
+                            max = 32,
+                            value = c(8, 32)
+                          ),
+                          sliderInput(
+                            inputId = "target_year_011_mpe",
+                            label = "Target year (cal BP)",
+                            min = 0, 
+                            max = 12310,
+                            value = c(0, 12310)
+                          )
                           ),
                           mainPanel(
                             plotOutput("plot_011_mpe")
-                            )
+                          )
                         )
-                        ),
-#WMDs021 EDA-------------------------------------------------------------------              
-               tabPanel(title = "Wiggle match Dates 021"),
-#WMDs022 EDA-------------------------------------------------------------------              
-               tabPanel(title = "Wiggle match Dates 022"),
-#WMDs023 EDA-------------------------------------------------------------------               
-               tabPanel(title = "Wiggle match Dates 023"),
-#WMDs024 EDA-------------------------------------------------------------------              
-               tabPanel(title = "Wiggle match Dates 024"),
-#WMDs025 EDA-------------------------------------------------------------------               
-               tabPanel(title = "Wiggle match Dates 025"),
-#Seqs031 EDA-------------------------------------------------------------------               
-               tabPanel(title = "Uniform Bound Sequences 031"),
-#Seqs032 EDA-------------------------------------------------------------------                 
-               tabPanel(title = "Uniform Bound Sequences 032"),
-#Seqs033 EDA-------------------------------------------------------------------                 
-               tabPanel(title = "Uniform Bound Sequences 033")
+               ),
+
+
+#Singles 011 Modelling---------------------------------------------------------
+               tabPanel(title = "Modelling")
+               ),
+
+
+#Start WMDs--------------------------------------------------------------------
+    navbarMenu(title = "14C wiggle-match dates",
+#WMDs EDA I: histograms -------------------------------------------------------            
+               tabPanel(title = "Accuracy histograms"),
+#WMDs EDA II: off-target magnitudes--------------------------------------------            
+               tabPanel(title = "Off-target magnitudes"),
+#WMDs EDA III: heatmaps--------------------------------------------------------              
+               tabPanel(title = "Heatmaps"),
+#WMDs MPE----------------------------------------------------------------------            
+               tabPanel(title = "Maximum permissible errors"),
+#WMDs Modelling----------------------------------------------------------------              
+               tabPanel(title = "Modelling")
                ),
 
 
 
 
 
-    navbarMenu(title = "Modelling")
+    navbarMenu(title = "Uniform bound sequence models",
+#Seqs EDA I: histograms -------------------------------------------------------            
+               tabPanel(title = "Accuracy histograms"),
+#Seqs EDA II: off-target magnitudes--------------------------------------------            
+               tabPanel(title = "Off-target magnitudes"),
+#Seqs EDA III: heatmaps--------------------------------------------------------              
+               tabPanel(title = "Heatmaps"),
+#Seqs MPE----------------------------------------------------------------------            
+               tabPanel(title = "Maximum permissible errors"),
+#Seqs Modelling----------------------------------------------------------------              
+               tabPanel(title = "Modelling")               
+               )
     
   )
 )
