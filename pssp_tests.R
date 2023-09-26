11549e205b81eb2981994e5e481acb7433a59617
Pete Jacobsson <pt.jacobsson@gmail.com>
2023-09-26 16:56:28 +0200

    Another bounce on getting pssp to work

---
+++ pssp_tests.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+
+pssp::plot_simulation_results_osc(range_to_plot = c(10, 15), hpd_area = "accuracy_68",
+                                  variable = "curve_uncert", rounding = 5,
+                                  xlab = "Calibration curve uncertainty")
