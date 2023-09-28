30551867c3158b64f9be07e842b82cfb0e3c622c
Pete Jacobsson <pt.jacobsson@gmail.com>
2023-09-28 16:36:14 +0200

    Added function, made mess

--- pssp_tests.R
+++ pssp_tests.R
@@ -2,4 +2,5 @@ library(tidyverse)
 
 pssp::plot_simulation_results_osc(range_to_plot = c(10, 15), hpd_area = "accuracy_68",
                                   variable = "curve_uncert", rounding = 5,
-                                  xlab = "Calibration curve uncertainty")
+                                  xlab = "Calibration curve uncertainty",
+                                  singles_results_csv = "single_cals_w_curve_uncert.csv")
